f824239c089351ae46ce7416190d47d1b3d3b551
cemen <frodo.work@gmail.com>
2025-06-05 17:12:14 +0300

    added order models: Order and OrderItem

---
+++ src/database/models/orders.py
@@ -0,0 +1,43 @@
+import enum
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    ForeignKey,
+    DateTime,
+    Enum,
+    DECIMAL
+)
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.functions import func
+
+from database import Base
+
+
+class OrderStatus(enum.Enum):
+    PENDING = "Pending"
+    PAID = "Paid"
+    CANCELED = "Canceled"
+
+
+class Order(Base):
+    __tablename__ = "orders"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    status = Column(Enum(OrderStatus), nullable=False, default=OrderStatus.PENDING)
+    total_amount = Column(DECIMAL(10, 2), nullable=True)
+
+    user = relationship("User", back_populates="orders")
+    order_items = relationship("OrderItem", back_populates="order")
+
+
+class OrderItem(Base):
+    __tablename__ = "order_items"
+    id = Column(Integer, primary_key=True, index=True)
+    order_id = Column(Integer, ForeignKey("orders.id"), nullable=False)
+    movie_id = Column(Integer, ForeignKey("movies.id"), nullable=False)
+    price_at_order = Column(DECIMAL(10, 2), nullable=False)
+
+    order = relationship("Order", back_populates="order_items")
+    movie = relationship("Movie", back_populates="order_items")
