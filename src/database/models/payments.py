4a9580c98e736bd38007087cc22fc88cbc3da36f
Denys Dikhtiar <dinis.dihtjar08@gmail.com>
2025-06-05 15:42:48 +0300

    Add new model for payment and resize payment value

--- src/database/models/payments.py
+++ src/database/models/payments.py
@@ -10,7 +10,7 @@ class PaymentStatus(PyEnum):
     REFUNDED = "refunded"
 
 
-class Payment(Base):
+class PaymentModel(Base):
     __tablename__ = "payments"
 
     id = Column(Integer, primary_key=True, index=True)
@@ -21,9 +21,22 @@ class Payment(Base):
     amount = Column(DECIMAL(10, 2), nullable=False)
     external_payment_id = Column(String, nullable=True)
 
-    # (Опціонально) зв’язки:
-    user = relationship("User", back_populates="payments")
-    order = relationship("Order", back_populates="payments")
+
+    user = relationship("UserModel", back_populates="payments")
+    order = relationship("OrderModel", back_populates="payments")
+    payment_items = relationship("PaymentItemModel", back_populates="payment")
 
     def __repr__(self):
         return f"<Payment id={self.id}, user={self.user_id}, order={self.order_id}, status={self.status}>"
+
+
+class PaymentItemModel(Base):
+    __tablename__ = "payment_items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    payment_id = Column(Integer, ForeignKey("payments.id"), nullable=False)
+    order_item_id = Column(Integer, ForeignKey("order_items.id"), nullable=False)
+    price_at_payment = Column(DECIMAL(10, 2), nullable=False)
+
+    payment = relationship("PaymentModel", back_populates="payment_items")
+    order_item = relationship("OrderItemModel", back_populates="payment_items")
