c0a698c41514110ac04f6f73c19f2b400aea3dc3
Denys Dikhtiar <dinis.dihtjar08@gmail.com>
2025-06-09 15:33:57 +0300

    remove get db for crud

--- src/crud/payments.py
+++ src/crud/payments.py
@@ -1,10 +1,7 @@
 from typing import Optional
 
-from fastapi import Depends
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-
-from database.deps import get_db
 
 from database.models.payments import PaymentModel
 from schemas.payments import PaymentCreateSchema, PaymentStatusSchema, PaymentUpdateSchema
@@ -12,7 +9,7 @@ from schemas.payments import PaymentCreateSchema, PaymentStatusSchema, PaymentUp
 
 async def create_payment(
     payment: PaymentCreateSchema,
-    db: AsyncSession = Depends(get_db)
+    db: AsyncSession
 ) -> PaymentModel:
     db_payment = PaymentModel(**payment.model_dump())
     db.add(db_payment)
@@ -23,16 +20,16 @@ async def create_payment(
 
 async def get_payment(
     payment_id: int,
-    db: AsyncSession = Depends(get_db)
+    db: AsyncSession
 ) -> Optional[PaymentModel]:
     result = await db.execute(select(PaymentModel).filter(PaymentModel.id == payment_id))
     return result.scalar_one_or_none()
 
 
 async def get_payments(
+    db: AsyncSession,
     skip: int = 0,
     limit: int = 100,
-    db: AsyncSession = Depends(get_db)
 ) -> list[PaymentModel]:
     result = await db.execute(select(PaymentModel).offset(skip).limit(limit))
     return list(result.scalars().all())
@@ -41,7 +38,7 @@ async def get_payments(
 async def update_payment(
     payment_id: int,
     payment: PaymentUpdateSchema,
-    db: AsyncSession = Depends(get_db)
+    db: AsyncSession
 ) -> Optional[PaymentModel]:
     db_payment = await get_payment(payment_id, db)
     if db_payment:
@@ -55,7 +52,7 @@ async def update_payment(
 
 async def delete_payment(
     payment_id: int,
-    db: AsyncSession = Depends(get_db)
+    db: AsyncSession
 ) -> Optional[PaymentModel]:
     db_payment = await get_payment(payment_id, db)
     if db_payment:
@@ -66,14 +63,14 @@ async def delete_payment(
 
 async def get_user_payments(
     user_id: int,
-    db: AsyncSession = Depends(get_db)
+    db: AsyncSession
 ) -> list[PaymentModel]:
     result = await db.execute(select(PaymentModel).where(PaymentModel.user_id == user_id))
     return list(result.scalars().all())
 
 
 async def get_all_payments(
-    db: AsyncSession = Depends(get_db),
+    db: AsyncSession,
     payment_status: Optional[PaymentStatusSchema] = None
 ) -> list[PaymentModel]:
     query = select(PaymentModel)
