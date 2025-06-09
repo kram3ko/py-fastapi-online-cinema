108f95d45cebb3a3abc484a7c97481ce8af9a666
Volodymyr Vinohradov <kram3ko@gmail.com>
2025-06-09 11:48:55 +0300

    Refactor JWT blacklist module for type hinting improvements and increase access token expiration time

--- src/security/token_manager.py
+++ src/security/token_manager.py
@@ -12,7 +12,7 @@ class JWTAuthManager(JWTAuthManagerInterface):
     A manager for creating, decoding, and verifying JWT access and refresh tokens.
     """
 
-    _ACCESS_KEY_TIMEDELTA_MINUTES = 60
+    _ACCESS_KEY_TIMEDELTA_MINUTES = 240
     _REFRESH_KEY_TIMEDELTA_MINUTES = 60 * 24 * 7
 
     def __init__(self, secret_key_access: str, secret_key_refresh: str, algorithm: str):
