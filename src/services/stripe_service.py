3b8b882ba98474d0895072895f01ab97641ced46
Denys Dikhtiar <dinis.dihtjar08@gmail.com>
2025-06-10 11:24:12 +0300

    rework session, to stripe session, get from there session url

--- src/services/stripe_service.py
+++ src/services/stripe_service.py
@@ -11,6 +11,9 @@ from schemas.payments import PaymentCreateSchema
 stripe_settings = get_settings()
 stripe.api_key = stripe_settings.STRIPE_SECRET_KEY
 
+SUCCESS_URL = f"{stripe_settings.FRONTEND_URL}:8000/api/v1/payments/history/"
+CANCEL_URL = f"{stripe_settings.FRONTEND_URL}:8000/api/v1/payments/history/"
+
 
 class StripeService:
     @staticmethod
@@ -18,23 +21,37 @@ class StripeService:
         try:
             amount_cents = int(payment_data.amount * 100)
 
-            intent = stripe.PaymentIntent.create(
-                amount=amount_cents,
-                currency=stripe_settings.STRIPE_CURRENCY,
+            session = stripe.checkout.Session.create(
+                payment_method_types=['card'],
+                line_items=[{
+                    'price_data': {
+                        'currency': stripe_settings.STRIPE_CURRENCY,
+                        'product_data': {
+                            'name': f'Order #{payment_data.order_id}',
+                        },
+                        'unit_amount': amount_cents,
+                    },
+                    'quantity': 1,
+                }],
+                mode='payment',
+                success_url=SUCCESS_URL,
+                cancel_url=CANCEL_URL,
                 metadata={
-                    "order_id": payment_data.order_id,
+                    'order_id': payment_data.order_id,
                 }
             )
 
             return {
-                "client_secret": intent.client_secret,
-                "payment_intent_id": intent.id
+                "payment_intent_id": session.id,
+                "payment_url": session.url,
             }
         except stripe.error.StripeError as e:  # type: ignore[attr-defined]
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail=str(e)
             )
+
+
 
     @staticmethod
     async def handle_webhook(payload: bytes, sig_header: str) -> Optional[dict]:
